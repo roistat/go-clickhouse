57775ac2ccf2bcbcc41f8bd3388a5e247edbd42f
Sergey Rudachenko <rs@roistat.com>
2016-07-04 15:38:01 +0300

    Cluster mutex

--- cluster.go
+++ cluster.go
@@ -1,11 +1,17 @@
 package clickhouse
 
+import (
+	"math/rand"
+	"sync"
+)
+
 type PingErrorEvent func(*Conn)
 
 type Cluster struct {
 	conn   []*Conn
 	active []*Conn
 	fail   PingErrorEvent
+	mx     sync.Mutex
 }
 
 func NewCluster(conn ...*Conn) *Cluster {
@@ -19,7 +25,13 @@ func (c *Cluster) OnFail(f PingErrorEvent) {
 }
 
 func (c *Cluster) Active() *Conn {
-	return c.active[0]
+	c.mx.Lock()
+	defer c.mx.Unlock()
+	l := len(c.active)
+	if l < 1 {
+		return nil
+	}
+	return c.active[rand.Intn(l)]
 }
 
 func (c *Cluster) CheckConnections() {
@@ -37,5 +49,7 @@ func (c *Cluster) CheckConnections() {
 		}
 	}
 
+	c.mx.Lock()
 	c.active = res
+	c.mx.Unlock()
 }
